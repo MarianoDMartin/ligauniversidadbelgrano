0511de2c43a23dd33c65f05989f3674650d9af34
Mariano Daniel Martin <MMartin@intranet.banelco.com.ar>
2017-09-13 15:26:11 -0300

    agrego codigniter

--- php/mensajeGmail.php
+++ php/mensajeGmail.php
@@ -17,13 +17,14 @@ if ($motivo=="1"){
 //Luego tenemos que iniciar la validación por SMTP:
 $mail->IsSMTP();
 $mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
 $mail->Host = "smtp.gmail.com"; // A RELLENAR. Aquí pondremos el SMTP a utilizar. Por ej. mail.midominio.com
 $mail->Username = "ub.comunidad.torneo@gmail.com"; // A RELLENAR. Email de la cuenta de correo. ej.info@midominio.com La cuenta de correo debe ser creada previamente. 
 $mail->Password = "torneo2017"; // A RELLENAR. Aqui pondremos la contraseña de la cuenta de correo
-$mail->Port = 465; // Puerto de conexión al servidor de envio. 
+$mail->Port = 587; // Puerto de conexión al servidor de envio. 
 $mail->From = "ub.comunidad.torneo@gmail.com"; // A RELLENARDesde donde enviamos (Para mostrar). Puede ser el mismo que el email creado previamente.
-$mail->FromName = "Pagin Web Torneo UB"; //A RELLENAR Nombre a mostrar del remitente. 
-$mail->AddAddress("ub.comunidad.torneo@gmail.com"); // Esta es la dirección a donde enviamos 
+$mail->FromName = "PWeb Torneo UB"; //A RELLENAR Nombre a mostrar del remitente. 
+$mail->AddAddress("mariano.d.martin@gmail.com"); // Esta es la dirección a donde enviamos 
 $mail->IsHTML(true); // El correo se envía como HTML 
 $mail->Subject = "PRUEBA Mensaje de $smotivo de paginaweb"; // Este es el titulo del email. 
 $body = "<h1>$smotivo de Pagina Web LigaUB</h1>"; 
@@ -35,7 +36,7 @@ $body .= "Mensaje: $mensaje";
 $exito = $mail->Body = $body; // Mensaje a enviar. $exito = $mail->Send(); // Envía el correo.
 if($exito){ 
 	echo "<script language='javascript'>
-	alert('Mensaje enviado, le estaremos contestando a su correo. Muchas gracias!!!');
+	alert('Mensaje enviado, le estaremos contestando a su correo. Muchas gracias!!..!');
 	window.location.href = '../index.html';
 	</script>";
 }else{
