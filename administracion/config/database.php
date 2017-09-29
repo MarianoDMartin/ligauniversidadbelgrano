2ef524a0af86da10eea8222303c2f0722eba5c9a
Mariano Martin <mariano.d.martin@gmail.com>
2017-09-29 15:54:37 -0300

    se agregan archivos a carpeta administracion

---
+++ administracion/config/database.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "driver"    =>"mysql",
+    "host"      =>"universys.site",
+    "user"      =>"apholos_dba",
+    "pass"      =>"dbainub",
+    "database"  =>"apholos_ligaub",
+    "charset"   =>"utf8"
+);
+?>
---
+++ administracion/config/global.php
@@ -0,0 +1,3 @@
+<?php
+//Más constantes de configuración
+?>
